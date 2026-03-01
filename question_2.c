af13741576b02102ec368b42ad0640bf4defbb57
Mihir Ranjan <connectmihirr@gmail.com>
2026-03-01 21:15:29 +0530

    FCFS + 2 Unit Context Switch

--- question_2.c
+++ question_2.c
@@ -1,66 +1,56 @@
 #include <stdio.h>
+
 int main()
 {
-    int i, j;
-    int process = 5;
-    int AT[5] = {0, 1, 2, 3, 4};
-    int BT[5] = {5, 7, 6, 2, 4};
-    int CT[5];
+    int i;
+    int n = 5;
+
+    int AT[5] = {0, 1, 2, 3, 4};  
+    int BT[5] = {5, 7, 6, 2, 4};  
+
+    int CT[5];   
+    int TAT[5];  
+    int WT[5];   
+
     int sum = 0;
-    for (i = 0; i < 5; i++)
+    int context_switch = 2;  
+    for (i = 0; i < n; i++)
     {
+        sum = sum + BT[i];
+
         if (i == 0)
-        {
-            sum = sum + BT[i] ;
-            CT[j++] = sum +2;
-        }
+            CT[i] = sum;  
         else
-        {
-            sum = sum + BT[i];
-            CT[j++] = sum +2*(i+1);
-        }
+            CT[i] = sum + context_switch * i;
     }
-    printf("Completion time:");
-    printf("\n");
-    for (i = 0; i < 5; i++)
-    {
+
+    printf("Completion Time:\n");
+    for (i = 0; i < n; i++)
         printf("%d ", CT[i]);
-    }
-    printf("\n");
 
-    int TAT[5];
-    for (i = 0; i < 5; i++)
-    {
+    for (i = 0; i < n; i++)
         TAT[i] = CT[i] - AT[i];
-    }
-    printf("Trun around time:");
-    printf("\n");
-    for (i = 0; i < 5; i++)
-    {
+
+    printf("\nTurnaround Time:\n");
+    for (i = 0; i < n; i++)
         printf("%d ", TAT[i]);
-    }
 
-    int WT[5];
-    for (i = 0; i < 5; i++)
-    {
+    
+    for (i = 0; i < n; i++)
         WT[i] = TAT[i] - BT[i];
-    }
-    printf("\n");
-    printf("Wating Time:");
-    printf("\n");
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d ", WT[i]);
-    }
 
+    printf("\nWaiting Time:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", WT[i]);
+
+    
     int sum_WT = 0;
-    for (i = 0; i < 5; i++)
-    {
-        sum_WT = sum_WT + WT[i];
-    }
-    printf("\n");
-    int avg_WT = sum_WT / 5;
-    printf("Average waiting time:%d", avg_WT);
+    for (i = 0; i < n; i++)
+        sum_WT += WT[i];
+
+    float avg_WT = (float)sum_WT / n;
+
+    printf("\nAverage Waiting Time: %.2f\n", avg_WT);
 
     return 0;
 }
